ab22838c699daf7b5060810c7d83ed8b36769178
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-03 14:14:31 -0400

    Adicionado código

---
+++ index.js
@@ -0,0 +1,30 @@
+class Produto{
+    constructor(nome,datacadastro,descricao,preco){
+    this.nome= nome;
+    this.datacadastro= datacadastro;
+    this.descricao= descricao;
+    this.preco= preco;
+
+}
+
+mostrar_produto(){
+    return this.nome + " " + this.datacadastro + " " + this.descricao+ " " + this.preco 
+}
+}
+  class ProdutoDestaque extends Produto{
+    constructor(nome,cadastro, descricao ,preco , imagem){
+    super(nome,cadastro, descricao ,preco)
+    this.imagem = imagem;
+    }
+    mostraprodutodestaque(){
+        return this.nome + this.cadastro + this.descricao + this.preco + this.imagem
+    
+    }
+}
+
+const produto = new Produto("Loubotin So Kate", "17/03/1991", "O icônico escarpin So Kate com elegância atemporal combina de forma brilhante discrição e sofisticação.", "3850") 
+console.log(produto.mostrar_produto());
+
+    let produtodestaque = new ProdutoDestaque("Loubotin So Kate","17/03/1991","O icônico escarpin So Kate com elegância atemporal combina de forma brilhante discrição e sofisticação.",8.000,"louboutin.jpg")
+    console.log(produtodestaque.mostraprodutodestaque())
+    
