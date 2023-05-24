12ec9576e32d1da45e84988fdc7510123f209216
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-24 13:23:03 -0400

    Update index.js
    Commit atualizado

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ mostrar_produto(){
     this.imagem = imagem;
     }
     mostraprodutodestaque(){
-        return this.nome + this.cadastro + this.descricao + this.preco + this.imagem
-    
+        return `
+        <div class="nero">${this.nome}</div>
+        <div class= "nero">${this.preco}</div>
+        <div class= "nero">${this.descricao}</div>
+        <div class= "nero">${this.datacadastro}</div>
+        
+    `;
+    //return this.nome + this.cadastro + this.descricao + this.preco + this.imagem;
     }
 }
 
 const produto = new Produto("Loubotin So Kate", "17/03/1991", "O icônico escarpin So Kate com elegância atemporal combina de forma brilhante discrição e sofisticação.", "3850") 
 console.log(produto.mostrar_produto());
 
-    let produtodestaque = new ProdutoDestaque("Loubotin So Kate","17/03/1991","O icônico escarpin So Kate com elegância atemporal combina de forma brilhante discrição e sofisticação.",8.000,"louboutin.jpg")
-    console.log(produtodestaque.mostraprodutodestaque())
-    
+    let produtodestaque = new ProdutoDestaque("Loubotin So Kate","17/03/1991","O icônico escarpin So Kate com elegância atemporal combina de forma brilhante discrição e sofisticação.",8.000,"louboutin.jpg");
+    console.log(produtodestaque.mostraprodutodestaque());
+    const nometf = document.getElementById("produto-destaque");
+    nometf.insertAdjacentHTML('afterbegin', produtodestaque.mostraprodutodestaque());
